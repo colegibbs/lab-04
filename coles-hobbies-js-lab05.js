f2e38088aac41be8105844f6706e6352928997a2
Cole Gibbs <colegibbs0@gmail.com>
2022-01-26 16:09:37 -0800

    added confirm

--- coles-hobbies-js-lab05.js
+++ coles-hobbies-js-lab05.js
@@ -1,15 +1,21 @@
 let firstName = prompt('What is your name?');
 let hobby = prompt('What is your favorite hobby?').toLowerCase().trim();
+let confirm = window.confirm('Would you like to learn about Cole\'s hobbies?');
 
-if(hobby === "jiu jitsu"){
-    alert('Cole also likes ' + hobby + '. Read on to learn some more!');
-}
-else if(hobby === "reading"){
-    alert('Cole also likes ' + hobby + '. Read on to learn some more!');
+if(confirm){
+    if(hobby === "jiu jitsu"){
+      alert('Cole also likes ' + hobby + '. Read on to learn some more!');
+    }
+    else if(hobby === "reading"){
+        alert('Cole also likes ' + hobby + '. Read on to learn some more!');
+    }
+    else{
+       alert('You don\'t have any of the same hobbies as Cole, but you should read on to learn more!');
+    }
 }
 else{
-    alert('You don\'t have any of the same hobbies as Cole, but you should read on to learn more!');
+    alert('THEN BE GONE!');
 }
 
 document.write('<h3> It was great to have you ' + firstName + '! </h3>');
-document.write('<h3>You could also check out my </h3><a href="https://github.com/colegibbs"><h3>Github</h3></a>');
+document.write('<h3>You could also check out my </h3><a href ="https://github.com/colegibbs"><h3>Github</h3></a>');
