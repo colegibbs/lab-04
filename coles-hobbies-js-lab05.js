da51346caeb862a5da2dfcf0b4628773bc24b5bb
Cole Gibbs <colegibbs0@gmail.com>
2022-01-27 18:33:39 -0800

    added content

--- coles-hobbies-js-lab05.js
+++ coles-hobbies-js-lab05.js
@@ -33,9 +33,9 @@ function promptUserForExitMessage() {
     document.write('<h3>You could also check out my </h3><a href ="https://github.com/colegibbs"><h3>Github</h3></a>');
 }
 
-function promptForPictures(message) {
-    let picAmount = prompt(message);
-    let defaultMessage = 'How awesome is Cole on a scale of 1 to 5?'
+function promptForPictures() {
+    let picAmount = prompt('How awesome is Cole on a scale of 1 to 5?');
+    let defaultMessage = 'How awesome is Cole on a scale of 1 to 5?';
     let error = 'Please enter a number!';
     if(picAmount === '') {
         promptForPictures(error + ' ' + defaultMessage);
@@ -53,27 +53,5 @@ function promptForPictures(message) {
 
 promptUserForExitMessage();
 promptUserForHobby();
-//let picMessage = 'How awesome is Cole on a scale of 1 to 5?';
-//promptForPictures(picMessage);
-
-//let firstName = prompt('What is your name?');
-//let hobby = prompt('What is your favorite hobby?').toLowerCase().trim();
-//let confirm = window.confirm('Would you like to learn about Cole\'s hobbies?');
-
-//if(confirm){
-//    if(hobby === "jiu jitsu"){
- //     alert('Cole also likes ' + hobby + '. Read on to learn some more!');
- //   }
-//    else if(hobby === "reading"){
-//        alert('Cole also likes ' + hobby + '. Read on to learn some more!');
-//    }
-//    else{
-//       alert('You don\'t have any of the same hobbies as Cole, but you should read on to learn more!');
-//    }
-///}
-//else{
-//    alert('THEN BE GONE!');
-//}
-
-//document.write('<h3> It was great to have you ' + firstName + '! </h3>');
-//document.write('<h3>You could also check out my </h3><a href ="https://github.com/colegibbs"><h3>Github</h3></a>');
+let picMessage = 'How awesome is Cole on a scale of 1 to 5?'
+promptForPictures();
